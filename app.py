1817716c0853d6cc389829c569b73eec7b21a5b7
liigo <liiigo@outlook.com>
2023-01-17 23:53:32 +0800

    finish treeSelChanged

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class MainFrame(wx.Frame):
     def __init__(self, parent, title):
         super().__init__(parent, title=title)
         self.SetSize((500, 300))
-        #self.textCtrls = [] # 树信息列表
+        self.textCtrls = [] # 右侧信息列表
         #self.itemData = []  # 被选中项目的值
         self.colNames = ['编号', '姓名', '手机号', '上级编号', '个人业绩']
         
@@ -26,7 +26,19 @@ class MainFrame(wx.Frame):
         self.Centre()
     
     def treeSelChanged(self, e):
-        pass 
+        treeItem = e.GetItem() 
+        treeData = self.tree.GetItemData(treeItem)
+
+        if not treeData:    # 点击root时treeData为0
+            return 
+        else:
+            for i in range(len(treeData) - 1):
+                self.textCtrls[i].SetValue(str(treeData[i+1]))
+            self.textCtrls[-1].SetValue(str(self.getItemTotalValue(treeItem)))
+    
+    def getItemTotalValue(self, treeItem):
+        treeData = self.tree.GetItemData(treeItem)
+        return 1
 
     def treeRightClick(self, e):
         pass 
@@ -72,20 +84,24 @@ class MainFrame(wx.Frame):
         gbs = wx.GridBagSizer(5, 2)
         btnBox = wx.BoxSizer(wx.HORIZONTAL)
         rightBox.Add(gbs, flag=wx.ALL, border=5)
-        rightBox.Add(btnBox, flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, border=15)
-        self.box.Add(rightBox, flag=wx.EXPAND | wx.ALL, border=10)
+        rightBox.Add(btnBox, flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, border=20)
+        self.box.Add(rightBox, flag=wx.ALL, border=10)
         
-        for i, name in enumerate(self.colNames):
+        for i, name in enumerate(self.colNames[1:]):
             gbs.Add(wx.StaticText(self.panel, label=name), (i, 0), flag=wx.ALIGN_CENTER_VERTICAL)
             # 不开启编辑，均为只读
-            gbs.Add(wx.TextCtrl(self.panel, size=(150, -1), style=wx.TE_READONLY), (i, 1), flag=wx.LEFT, border=8)        
-        gbs.Add(wx.StaticText(self.panel, label='总个人业绩'), (5, 0))
-        #gbs.Add(wx.StaticText(self.panel, label=str(self.getTotalValue())))
+            tc = wx.TextCtrl(self.panel, size=(150, -1), style=wx.TE_READONLY)
+            self.textCtrls.append(tc)
+            gbs.Add(tc, (i, 1), flag=wx.LEFT, border=8)   
         
-        #btnBox.Add(wx.StaticText(self.panel, label=''))
+        # 总个人业绩永远为只读     
+        gbs.Add(wx.StaticText(self.panel, label='总个人业绩'), (5, 0))
+        tc = wx.TextCtrl(self.panel, size=(150, -1), style=wx.TE_READONLY)
+        self.textCtrls.append(tc)
+        gbs.Add(tc, (5, 1), flag=wx.LEFT, border=8)
+        
         btnBox.Add(wx.Button(self.panel, wx.ID_EDIT, '编辑', size=(60, 30)), flag=wx.LEFT | wx.RIGHT, border=10)
         btnBox.Add(wx.Button(self.panel, wx.ID_SAVE, '保存', size=(60, 30)), flag=wx.LEFT | wx.RIGHT, border=10)
-        #btnBox.Add(wx.StaticText(self.panel, label=''))
 
 def main():
     app = wx.App()
